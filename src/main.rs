1d56eb5846db4f13f39ec0d912fe129e5b5f5159
Eran Boodnero <eboodnero@gmail.com>
2024-07-02 13:56:28 -0700

    Added health check route

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use axum::Router;
+use axum::{routing::get, Router};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().fallback_service(ServeDir::new("web-folder"));
+    let app = Router::new()
+        .route("/health", get(|| async { "OK" }))
+        .fallback_service(ServeDir::new("web-folder"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on 3000");
