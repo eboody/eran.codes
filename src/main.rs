bf89204ad64ea2264ee53a7be7e122b0a51f16a7
Eran Boodnero <eboodnero@gmail.com>
2024-07-02 13:52:16 -0700

    fixed the directory im server as per the docker file

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().fallback_service(ServeDir::new("web-folder/static"));
+    let app = Router::new().fallback_service(ServeDir::new("web-folder"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on 3000");
     axum::serve(listener, app).await.unwrap();
 }
