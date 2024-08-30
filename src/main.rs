a32b29288fcd4961c15f3b0b05714b61f24d9344
Eran Boodnero <eboodnero@gmail.com>
2024-08-30 08:07:46 -0700

    Changed logo src to eran.codes/logo.png

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() {
         .route("/health", get(|| async { "OK" }))
         .fallback_service(ServeDir::new("web-folder"));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Listening on 3000");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3003").await.unwrap();
+    println!("Listening on 3003");
     axum::serve(listener, app).await.unwrap();
 }
