b9840017dce35711050c8c3239a6f0c3655574a9
Eran Boodnero <eboodnero@gmail.com>
2026-01-04 08:14:03 -0800

    add live-reload feature

--- crates/http/src/lib.rs
+++ crates/http/src/lib.rs
@@ -18,11 +18,24 @@ impl State {
 }
 
 pub fn router(state: State) -> Router {
-    Router::new()
+    let router = Router::new()
         .route("/", get(crate::handlers::home))
         .route("/partials/ping", get(crate::handlers::ping_partial))
         .route("/health", get(crate::handlers::health))
         .nest_service("/static", ServeDir::new("crates/http/static"))
-        .layer(tower_livereload::LiveReloadLayer::new())
-        .with_state(state)
+        .with_state(state);
+
+    let router = {
+        #[cfg(all(debug_assertions, feature = "live-reload"))]
+        {
+            router.layer(tower_livereload::LiveReloadLayer::new())
+        }
+
+        #[cfg(not(all(debug_assertions, feature = "live-reload")))]
+        {
+            router
+        }
+    };
+
+    router
 }
