78131c64e6a736185bce75f53452c04884a9d7f7
Eran Boodnero <eboodnero@gmail.com>
2026-01-12 06:33:02 -0800

    use eran.codes.png for favicon

--- crates/http/src/views/page.rs
+++ crates/http/src/views/page.rs
@@ -14,10 +14,8 @@ impl Render for Layout<'_> {
                     meta charset="utf-8";
                     meta name="viewport" content="width=device-width, initial-scale=1";
                     title { (self.title) }
-                    link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png";
-                    link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png";
-                    link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png";
-                    link rel="icon" href="/static/favicon.ico";
+                    link rel="icon" type="image/png" sizes="1024x1024" href="/static/eran.codes.png";
+                    link rel="apple-touch-icon" sizes="1024x1024" href="/static/eran.codes.png";
                     link rel="stylesheet" href="/static/app.css";
                     script type="module" src="https://cdn.jsdelivr.net/gh/starfederation/datastar@1.0.0-RC.7/bundles/datastar.js" {}
                     script src="/static/css-scope-inline.js" {}
