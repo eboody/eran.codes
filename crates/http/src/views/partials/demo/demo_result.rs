cf31cda1a0a8b98c3793ff1cbab3c95b72ed3596
Eran Boodnero <eboodnero@gmail.com>
2026-02-04 08:07:12 -0800

    use empty state in demo placeholders

--- crates/http/src/views/partials/demo/demo_result.rs
+++ crates/http/src/views/partials/demo/demo_result.rs
@@ -1,6 +1,8 @@
 use bon::Builder;
 use maud::Render;
 
+use crate::views::partials::EmptyState;
+
 #[derive(Clone, Debug, Builder)]
 pub struct DemoResultPlaceholder {
     pub target_id: String,
@@ -11,7 +13,10 @@ impl Render for DemoResultPlaceholder {
     fn render(&self) -> maud::Markup {
         maud::html! {
             div id=(self.target_id.as_str()) class="demo-result muted" {
-                (self.message.as_str())
+                (EmptyState::builder()
+                    .message(self.message.clone())
+                    .build()
+                    .render())
             }
         }
     }
